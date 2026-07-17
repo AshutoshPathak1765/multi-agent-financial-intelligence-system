2ec4b7eff7067a607ccd0952d03b0ec7623eefe5
AshutoshPathak1765 <ashutoshpathak1765@gmail.com>
2026-07-17 12:21:05 +0530

    added frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,8 +41,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://multi-agent-financial-intelligence.vercel.app/"
-        ],
+        "https://multi-agent-financial-intelligence.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
